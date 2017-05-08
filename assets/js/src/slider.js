8f6a37fe451fdd2ce823fed1e873f632e6c1db4c
Mihai Varsandan <themike2993@gmail.com>
2017-05-08 20:37:40 +0100

    started building slider

---
+++ assets/js/src/slider.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	var slider = function(){
+		var $slider = $('.project-container .slider');
+		var $prev_button = $slider.find('.prev');
+		var $next_button = $slider.find('.next');
+
+		$next_button.click(function(){
+			var $current_slide = $(this).parent('.slider').find('.slide.active');
+			var $next_slide = $current_slide.next();
+
+			$current_slide.removeClass('active');
+			$next_slide.addClass('active');
+		});
+	}
+
+	slider();
+});
