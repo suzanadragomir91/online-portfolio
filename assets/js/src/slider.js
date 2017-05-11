fa3b34ccad9296c1775b01cfdbec0a750fd08261
Suzana Dragomir <dragomirsuzana@gmail.com>
2017-05-11 11:20:33 +0300

    jquery for slider

--- assets/js/src/slider.js
+++ assets/js/src/slider.js
@@ -4,13 +4,28 @@ $(document).ready(function(){
 		var $prev_button = $slider.find('.prev');
 		var $next_button = $slider.find('.next');
 
+
 		$next_button.click(function(){
 			var $current_slide = $(this).parent('.slider').find('.slide.active');
 			var $next_slide = $current_slide.next();
 
 			$current_slide.removeClass('active');
 			$next_slide.addClass('active');
+
+
+
 		});
+
+			$prev_button.click(function(){
+				var $current_slide = $(this).parent('.slider').find('.slide.active');
+				var $prev_slide = $current_slide.prev();
+
+				$current_slide.removeClass('active');
+				$prev_slide.addClass('active');
+
+
+		});
+
 	}
 
 	slider();
