9eae8067f1d188cd0a1d260d545dcc8762f12809
Mihai Varsandan <themike2993@gmail.com>
2017-05-15 19:55:59 +0100

    finished slider

--- assets/js/src/slider.js
+++ assets/js/src/slider.js
@@ -1,46 +1,38 @@
 $(document).ready(function(){
 	var slider = function(){
-		var $slider = $('.project-container .slider');
-		var $prev_button = $slider.find('.prev');
-		var $next_button = $slider.find('.next');
+		var $slider 		= $('.project-container .slider');
+		var $prev_button 	= $slider.find('.prev');
+		var $next_button 	= $slider.find('.next');
 
+		$prev_button.click(function(){
+			var $current_slide 	= $(this).parent('.slider').find('.slide.active');
+			var $prev_slide 	= $current_slide.prev();
+			var slide_count 	= $current_slide.index();
 
-		$next_button.click(function(){
-			var $current_slide = $(this).parent('.slider').find('.slide.active');
-			var $next_slide = $current_slide.next();
-
-			$current_slide.removeClass('active');
-			$next_slide.addClass('active');
-
-
-
-		});
-
-			$prev_button.click(function(){
-				var $current_slide = $(this).parent('.slider').find('.slide.active');
-				var $prev_slide = $current_slide.prev();
-
+			if (slide_count == 0) {
+				$current_slide.removeClass('active');
+				$(this).parent('.slider').find('.slide').last().addClass('active');
+			} else {
 				$current_slide.removeClass('active');
 				$prev_slide.addClass('active');
-
-
+			}
 		});
 
-			var current_slide = 1;
-			var next_slide = 2;
+		$next_button.click(function(){
+			var $current_slide 	= $(this).parent('.slider').find('.slide.active');
+			var $next_slide 	= $current_slide.next();
+			var slide_length 	= $(this).parent('.slider').find('.slide').length-1;
+			var slide_count 	= $current_slide.index();
 
-			function startSlider() {
-				count = $(".slides > img").size();
-
-				if(current_slide > count){
-					next_slide = 1;
-					current_slide = 1;
-				}
-
-					current_slide = next_slide;
-					next_slide = next_slide + 1;
-
+			if (slide_count == slide_length) {
+				$current_slide.removeClass('active');
+				$(this).parent('.slider').find('.slide').first().addClass('active');
+			} else {
+				$current_slide.removeClass('active');
+				$next_slide.addClass('active');
+			}
+		});
+	}
 
 	slider();
-	startSlider();
 });
